207ba6cac741101da387919c14eb029080cfdc02
funtik0310 <funtik03101996@gmail.com>
2024-11-12 12:00:16 +0300

    Add files via upload

---
+++ module_11_3.py
@@ -0,0 +1,22 @@
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+
+    methods = [m for m in dir(obj) if callable(getattr(obj, m))]
+
+    module = obj.__class__.__module__
+
+    dict_int = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': module,
+
+    }
+
+    return dict_int
+
+
+number_info = introspection_info(42)
+print(number_info)
